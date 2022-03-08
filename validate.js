fbfc254c69a0e1f8abe6cdcb1915ebd3e3119600
ksigei <sigeikiprono4@gmail.com>
2022-03-08 15:19:12 +0300

    Form validate

---
+++ validate.js
@@ -0,0 +1,29 @@
+function validateMail() {
+    const form = document.getElementById('myForm');
+    const email = form.elements.email.value;
+    const re = /@/g;
+    const lower = email.toLowerCase();
+  
+    if (re.test(String(email).toLowerCase()) === true) {
+      if (lower === email) { 
+        form.submit(); 
+      } else {
+        form.addEventListener('submit', (event) => {
+          event.preventDefault(); 
+          const error = document.createElement('msgSection'); 
+          error.textContent = 'Oops the form is not sent, change your email to lowercase and retry.'; 
+          error.id = 'errorMessage';
+          setTimeout(() => {
+            error.classList.add('fadeOut');
+          }, 500);
+          form.appendChild(error);
+          setTimeout(() => { 
+            form.removeChild(document.getElementById('errorMessage'));
+          }, 3500);
+        });
+      }
+    }
+  }
+  
+  validateMail();
+  
