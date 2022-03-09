3795c0f632ec3e7f2f35eff66a9aba4982a5d1f8
ksigei <sigeikiprono4@gmail.com>
2022-03-09 20:02:35 +0300

    Solve linter errors

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,18 +2,20 @@ const openEls = document.querySelectorAll('[data-open]');
 const closeEls = document.querySelectorAll('[data-close]');
 const isVisible = 'is-visible';
 
-for (const el of openEls) {
+openEls.forEach((el) => {
   el.addEventListener('click', function () {
     const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible);
+    document
+      .getElementById(modalId)
+      .classList.add(isVisible);
   });
-}
+});
 
-for (const el of closeEls) {
+closeEls.forEach((el) => {
   el.addEventListener('click', function () {
     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
   });
-}
+});
 
 document.addEventListener('click', (e) => {
   if (e.target === document.querySelector('.modal.is-visible')) {
