3f9e754d2a2fa1a52479b5fa068c4036bc63f6cd
ksigei <sigeikiprono4@gmail.com>
2022-03-21 09:54:10 +0300

    Resolve objects issues

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,132 +1,109 @@
-let htmlElements = '';
-
 const projects = [
   {
-    id: 'projects-wrapper',
+    name: 'World crypto summit',
+    demo: 'https://ksigei.github.io/portfolio2/',
+    source: 'https://github.com/ksigei/portfolio2',
+    html: 'HTML/CSS',
+    ruby: 'Ruby on Rails',
+    js: 'JavaScript',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    img: './images/carousel.svg',
+  }, {
     name: 'Project name goes here',
     demo: 'https://ksigei.github.io/portfolio2/',
     source: 'https://github.com/ksigei/portfolio2',
-    langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
-    },
-  },
-  {
-    id: 'projects-wrapper',
+    html: 'HTML/CSS',
+    ruby: 'Ruby on Rails',
+    js: 'JavaScript',
+  }, {
     name: 'Project name goes here',
     demo: 'https://ksigei.github.io/portfolio2/',
     source: 'https://github.com/ksigei/portfolio2',
-    langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
-    },
-  },
-  {
-    id: 'projects-wrapper',
+    html: 'HTML/CSS',
+    ruby: 'Ruby on Rails',
+    js: 'JavaScript',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    img: './images/speakers/armstrong.jpeg',
+  }, {
     name: 'Project name goes here',
     demo: 'https://ksigei.github.io/portfolio2/',
     source: 'https://github.com/ksigei/portfolio2',
-    langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
-    },
-  },
-  {
-    id: 'projects-wrapper',
+    html: 'HTML/CSS',
+    ruby: 'Ruby on Rails',
+    js: 'JavaScript',
+  }, {
     name: 'Project name goes here',
     demo: 'https://ksigei.github.io/portfolio2/',
     source: 'https://github.com/ksigei/portfolio2',
-    langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
-    },
-  },
-  {
-    id: 'projects-wrapper',
+    html: 'HTML/CSS',
+    ruby: 'Ruby on Rails',
+    js: 'JavaScript',
+  }, {
     name: 'Project name goes here',
     demo: 'https://ksigei.github.io/portfolio2/',
     source: 'https://github.com/ksigei/portfolio2',
-    langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
-    },
-  },
-  {
-    id: 'projects-wrapper',
-    name: 'Project name goes here',
-    demo: 'https://ksigei.github.io/portfolio2/',
-    source: 'https://github.com/ksigei/portfolio2',
-    langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
-    },
-  },
-  {
-    id: 'projects-wrapper',
-    name: 'Project name goes here',
-    demo: 'https://ksigei.github.io/portfolio2/',
-    source: 'https://github.com/ksigei/portfolio2',
-    langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
-    },
+    html: 'HTML/CSS',
+    ruby: 'Ruby on Rails',
+    js: 'JavaScript',
   },
 ];
 
-for (let i = 0; i <= 6; i += 1) {
-  htmlElements
-   += ` 
-   <div class="modal" id="modal2" data-animation="mixInAnimations">
-   <div class="modal-dialog">
-     <header class="modal-header">
-       <button class="close-modal" aria-label="close modal" data-close>
-         ✕  
-       </button>
-     </header>
-     <section class="modal-content">
-       <h3>Project name goes here</h3> 
-       <div class="list-languages">
-           <ul>
-               <li class="langs-li-dsk cursor"><p>${projects[projects.length - 1].html}</p></li>
-               <li class="langs-li-dsk cursor"><p>${projects[projects.length - 1].ruby}</p></li>
-               <li class="langs-li-dsk cursor"><p>${projects[projects.length - 1].js}</p></li>
-           </ul>
-       </div>
-       <div class="modal-carousel">
-           <img src="./images/carousel.svg" alt="">
-       </div>
-       
-       <div class="modal-description">
-           <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
-           </p>
+const cards_container = document.getElementById('modal_container');
 
-       </div>
-       
-       <div class="modal-btns">
-           <div class="btn-1">
-               <a href="#">
-                   <p>See Live <span><img src="./images/resume.png" alt="image"/></span></p>
-               </a>
-           </div>
-           <div class="btn-1">
-               <a href="#">
-                   <p>See Source <span><img src="./images/github.png" alt="image"/></span></p>
-               </a>
-           </div>
-       </div>
-       </section>
+projects.forEach((data) => {
+  const card = document.createElement('div');
+  card.classList = 'modal';
+
+  const content = `<div id="modal2" data-animation="mixInAnimations">
+  <div class="modal-dialog">
+   <header class="modal-header">
+     <button class="close-modal" aria-label="close modal" data-close>
+       ✕  
+     </button>
+   </header>
+   <section class="modal-content">
+     <h3>${data.name}</h3> 
+     <div class="list-languages">
+         <ul>
+             <li class="langs-li-dsk cursor"><p>${data.html}</p></li>
+             <li class="langs-li-dsk cursor"><p>${data.ruby}</p></li>
+             <li class="langs-li-dsk cursor"><p>${data.js}</p></li>
+         </ul>
+     </div>
+     <div class="modal-carousel">
+         <img src="${data.img}" alt="">
+     </div>
      
-   </div>
+     <div class="modal-description">
+         <p>
+         ${data.description}
+         </p>
+
+     </div>
+     
+     <div class="modal-btns">
+         <div class="btn-1">
+             <a href="#">
+                 <p>See Live <span><img src="./images/resume.png" alt="image"/></span></p>
+             </a>
+         </div>
+         <div class="btn-1">
+             <a href="#">
+                 <p>See Source <span><img src="./images/github.png" alt="image"/></span></p>
+             </a>
+         </div>
+     </div>
+     </section>
+   
+ </div>
 </div>`;
-}
+
+container.innerHTML += content;
+});
+
+
+
+
 
 const openEls = document.querySelectorAll('[data-open]');
 const closeEls = document.querySelectorAll('[data-close]');
@@ -160,5 +137,5 @@ document.addEventListener('keyup', (e) => {
   }
 });
 
-const container = document.getElementById('#projects');
-container.innerHTML = htmlElements;
+// const container = document.getElementById('#projects');
+// container.innerHTML = htmlElements;
