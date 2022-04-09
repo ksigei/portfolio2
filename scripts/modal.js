0b03373407474e49d298cde239646691d1409041
ksigei <sigeikiprono4@gmail.com>
2022-04-09 16:57:45 +0300

    Modify the add project

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,164 +1,158 @@
-let htmlElements = '';
+// let htmlElements = '';
 
-const projects = [
+const htmlArrays = [
   {
-    id: 'projects-wrapper',
-    name: 'Project name goes here',
-    demo: 'https://ksigei.github.io/portfolio2/',
-    source: 'https://github.com/ksigei/portfolio2',
+    image: "./images/screenshots/ss4.png",
+    name: "World crypto-currency summit",
+    demo: "https://ksigei.github.io/portfolio2/",
+    source: "https://github.com/ksigei/portfolio2",
+    description: "Html, css capstone project",
     langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
+      html: "HTML/CSS",
+      ruby: "Ruby on Rails",
+      js: "JavaScript",
     },
   },
   {
-    id: 'projects-wrapper',
-    name: 'Project name goes here',
-    demo: 'https://ksigei.github.io/portfolio2/',
-    source: 'https://github.com/ksigei/portfolio2',
+    image: "./images/screenshots/ss4.png",
+    name: "Project name goes here",
+    demo: "https://ksigei.github.io/portfolio2/",
+    source: "https://github.com/ksigei/portfolio2",
+    description: "Html, css capstone project",
     langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
+      html: "HTML/CSS",
+      ruby: "Ruby on Rails",
+      js: "JavaScript",
     },
   },
   {
-    id: 'projects-wrapper',
-    name: 'Project name goes here',
-    demo: 'https://ksigei.github.io/portfolio2/',
-    source: 'https://github.com/ksigei/portfolio2',
+    image: "./images/screenshots/ss4.png",
+    name: "Project name goes here",
+    demo: "https://ksigei.github.io/portfolio2/",
+    source: "https://github.com/ksigei/portfolio2",
+    description: "Html, css capstone project",
     langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
+      html: "HTML/CSS",
+      ruby: "Ruby on Rails",
+      js: "JavaScript",
     },
   },
   {
-    id: 'projects-wrapper',
-    name: 'Project name goes here',
-    demo: 'https://ksigei.github.io/portfolio2/',
-    source: 'https://github.com/ksigei/portfolio2',
+    image: "./images/screenshots/ss4.png",
+    name: "Project name goes here",
+    demo: "https://ksigei.github.io/portfolio2/",
+    source: "https://github.com/ksigei/portfolio2",
+    description: "Html, css capstone project",
     langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
+      html: "HTML/CSS",
+      ruby: "Ruby on Rails",
+      js: "JavaScript",
     },
   },
   {
-    id: 'projects-wrapper',
-    name: 'Project name goes here',
-    demo: 'https://ksigei.github.io/portfolio2/',
-    source: 'https://github.com/ksigei/portfolio2',
+    image: "./images/screenshots/ss4.png",
+    name: "Project name goes here",
+    demo: "https://ksigei.github.io/portfolio2/",
+    source: "https://github.com/ksigei/portfolio2",
+    description: "Html, css capstone project",
     langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
+      html: "HTML/CSS",
+      ruby: "Ruby on Rails",
+      js: "JavaScript",
     },
   },
   {
-    id: 'projects-wrapper',
-    name: 'Project name goes here',
-    demo: 'https://ksigei.github.io/portfolio2/',
-    source: 'https://github.com/ksigei/portfolio2',
+    image: "./images/screenshots/ss4.png",
+    name: "Project name goes here",
+    demo: "https://ksigei.github.io/portfolio2/",
+    source: "https://github.com/ksigei/portfolio2",
+    description: "Html, css capstone project",
     langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
+      html: "HTML/CSS",
+      ruby: "Ruby on Rails",
+      js: "JavaScript",
     },
   },
   {
-    id: 'projects-wrapper',
-    name: 'Project name goes here',
-    demo: 'https://ksigei.github.io/portfolio2/',
-    source: 'https://github.com/ksigei/portfolio2',
+    image: "./images/screenshots/ss4.png",
+    name: "Project name goes here",
+    demo: "https://ksigei.github.io/portfolio2/",
+    source: "https://github.com/ksigei/portfolio2",
+    description: "Html, css capstone project",
     langs: {
-      html: 'HTML/CSS',
-      ruby: 'Ruby on Rails',
-      js: 'JavaScript',
+      html: "HTML/CSS",
+      ruby: "Ruby on Rails",
+      js: "JavaScript",
     },
   },
 ];
-
-for (let i = 0; i <= 6; i += 1) {
-  htmlElements
-   += ` 
-   <div class="modal" id="modal2" data-animation="mixInAnimations">
-   <div class="modal-dialog">
-     <header class="modal-header">
-       <button class="close-modal" aria-label="close modal" data-close>
-         ✕  
-       </button>
-     </header>
+htmlArrays.forEach((item) => {
+ const html = ` 
      <section class="modal-content">
-       <h3>Project name goes here</h3> 
+       <h3>${item.name}</h3> 
        <div class="list-languages">
            <ul>
-               <li class="langs-li-dsk cursor"><p>${projects[projects.length - 1].html}</p></li>
-               <li class="langs-li-dsk cursor"><p>${projects[projects.length - 1].ruby}</p></li>
-               <li class="langs-li-dsk cursor"><p>${projects[projects.length - 1].js}</p></li>
+               <li class="langs-li-dsk cursor"><p>${item.langs.html}</p></li>
+               <li class="langs-li-dsk cursor"><p>${item.langs.js}</p></li>
+               <li class="langs-li-dsk cursor"><p>${item.langs.ruby}</p></li>
            </ul>
        </div>
        <div class="modal-carousel">
-           <img src="./images/carousel.svg" alt="">
+           <img src="${item.image}" alt="">
        </div>
        
        <div class="modal-description">
            <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
+           ${item.description}
            </p>
 
        </div>
        
        <div class="modal-btns">
            <div class="btn-1">
-               <a href="#">
+               <a href="${item.demo}">
                    <p>See Live <span><img src="./images/resume.png" alt="image"/></span></p>
                </a>
            </div>
            <div class="btn-1">
-               <a href="#">
+               <a href="${item.source}">
                    <p>See Source <span><img src="./images/github.png" alt="image"/></span></p>
                </a>
            </div>
        </div>
-       </section>
-     
-   </div>
-</div>`;
-}
+       </section>`;
+const projects_view = document.getElementById("m_dialog");
+projects_view.innerHTML = html;
+});
 
-const openEls = document.querySelectorAll('[data-open]');
-const closeEls = document.querySelectorAll('[data-close]');
-const isVisible = 'is-visible';
+
+const openEls = document.querySelectorAll("[data-open]");
+const closeEls = document.querySelectorAll("[data-close]");
+const isVisible = "is-visible";
 
 openEls.forEach((el) => {
-  el.addEventListener('click', function () {
+  el.addEventListener("click", function () {
     const modalId = this.dataset.open;
-    document
-      .getElementById(modalId)
-      .classList.add(isVisible);
+    document.getElementById(modalId).classList.add(isVisible);
   });
 });
 
 closeEls.forEach((el) => {
-  el.addEventListener('click', function () {
+  el.addEventListener("click", function () {
     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
   });
 });
 
-document.addEventListener('click', (e) => {
-  if (e.target === document.querySelector('.modal.is-visible')) {
-    document.querySelector('.modal.is-visible').classList.remove(isVisible);
+document.addEventListener("click", (e) => {
+  if (e.target === document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
   }
 });
 
-document.addEventListener('keyup', (e) => {
+document.addEventListener("keyup", (e) => {
   // if we press the ESC
-  if (e.key === 'Escape' && document.querySelector('.modal.is-visible')) {
-    document.querySelector('.modal.is-visible').classList.remove(isVisible);
+  if (e.key === "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
   }
 });
 
-const container = document.getElementById('#projects');
-container.innerHTML = htmlElements;
